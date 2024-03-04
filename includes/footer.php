09dc4590e7c3c16b86444aae4ec8030f61121064
mooizahmed102 <amoiz6263@gmail.com>
2024-03-04 11:21:55 -0800

    first commit

--- includes/footer.php
+++ includes/footer.php
@@ -166,3 +166,16 @@ St. Petersburg , FL 33702</p>
                     <i class="fas fa-chevron-up"></i>
                 </a>
             </footer>
+            <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/65e61db29131ed19d974e5b0/1ho5e80p1';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
